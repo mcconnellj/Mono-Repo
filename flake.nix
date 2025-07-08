015a7fb66e2c76cb4cace5b9f8ec5a6c5b51f64d
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-07-08 13:15:31 -0400

    test

--- flake.nix
+++ flake.nix
@@ -4,104 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    mach-nix.url = "github:DavHau/mach-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        # Manually specify Python packages you need here, e.g.
-        pythonPackages = with pkgs.python311Packages; [
-          # Add the Python packages here you want, e.g.
-          aiohttp
-          aiosignal
-          annotated-types
-          anyio
-          async-timeout
-          attrs
-          audioplayer
-          av
-          certifi
-          cffi
-          charset-normalizer
-          colorama
-          coloredlogs
-          ctranslate2
-          distro
-          EasyProcess
-          entrypoint2
-          faster-whisper
-          ffmpeg-python
-          filelock
-          flatbuffers
-          frozenlist
-          fsspec
-          future
-          h11
-          httpcore
-          httpx
-          huggingface-hub
-          humanfriendly
-          idna
-          Jinja2
-          llvmlite
-          MarkupSafe
-          more-itertools
-          MouseInfo
-          mpmath
-          mss
-          multidict
-          networkx
-          numba
-          numpy
-          onnxruntime
-          openai
-          packaging
-          Pillow
-          protobuf
-          pycparser
-          pydantic
-          pydantic_core
-          PyGetWindow
-          PyMsgBox
-          pynput
-          pyperclip
-          PyQt5
-          PyQt5-Qt5
-          PyQt5-sip
-          pyreadline3
-          PyRect
-          pyscreenshot
-          PyScreeze
-          python-dotenv
-          pytweening
-          PyYAML
-          regex
-          requests
-          six
-          sniffio
-          sounddevice
-          soundfile
-          sympy
-          tiktoken
-          tokenizers
-          tqdm
-          typing_extensions
-          urllib3
-          webrtcvad-wheels
-          yarl
-          whisper
-        ];
-
-        pythonEnv = pkgs.python311.withPackages (ps: pythonPackages);
+        mach = mach-nix.lib.${system};
+        whisperWriterReqs = builtins.readFile ./whisper-writer/requirements.txt;
       in {
+        # Expose a python package output with Python 3.11 environment
         packages = {
-          python = pythonEnv;
+          python = mach.mkPythonShell {
+            python = pkgs.python311;
+            requirements = whisperWriterReqs;
+          };
         };
 
-        devShell = pkgs.mkShell {
-          buildInputs = [ pythonEnv ];
-        };
+        devShell = packages.python;
       });
 }
