5e133c3d76332924a60d6c27c9337e698713081b
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-07-08 13:15:37 -0400

    test

--- flake.nix
+++ flake.nix
@@ -4,24 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    mach-nix.url = "github:DavHau/mach-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        mach = mach-nix.lib.${system};
-        whisperWriterReqs = builtins.readFile ./whisper-writer/requirements.txt;
+        requirementsFile = ./requirements.txt;
       in {
-        # Expose a python package output with Python 3.11 environment
-        packages = {
-          python = mach.mkPythonShell {
-            python = pkgs.python311;
-            requirements = whisperWriterReqs;
-          };
-        };
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.python311 pkgs.python311Packages.pip ];
 
-        devShell = packages.python;
+          shellHook = ''
+            echo "Installing pip packages from requirements.txt..."
+            pip install --upgrade pip
+            pip install -r ${toString requirementsFile}
+          '';
+        };
       });
 }
